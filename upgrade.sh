6da9b3cd74bd9eb5bb0605701d2b6dcaaa1bf7ba
Sandae Macalalag <sandaemc@gmail.com>
2017-02-24 18:19:20 +0800

    Upgrade!

---
+++ upgrade.sh
@@ -0,0 +1,17 @@
+#/usr/bin/env bash
+
+cd validator; git pull origin master; 
+cd -
+
+cd reader; git pull origin master;
+cd -
+
+cd renderer; git pull origin master;
+cd -
+
+cd courses/ca-school-php-introduction; git pull origin getting-started;
+cd -
+
+git add . -A
+git commit -m "Upgrade!"
+git push

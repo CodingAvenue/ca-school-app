952e869a56e84ced76d9a105138fb1fad3095056
Sandae Macalalag <sandaemc@gmail.com>
2017-03-08 14:44:00 +0800

    Use images instead

--- upgrade.sh
+++ upgrade.sh
@@ -1,18 +1,6 @@
 #/usr/bin/env bash
 
-cd validator; git pull origin master; 
-cd -
-
-cd reader; git pull origin master;
-cd -
-
 cd renderer; git pull origin master;
-cd -
-
-cd sandbox; git pull origin master;
-cd -
-
-cd platform; git pull origin master;
 cd -
 
 cd courses/ca-school-php-introduction; git pull origin getting-started;
